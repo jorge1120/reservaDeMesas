fba695a0bef1963626fede36f23ed474bcac846e
jorge1120 <jorge.lopez19@itca.edu.sv>
2022-06-09 16:50:22 -0600

    reservation class

--- source code/reservation.php
+++ source code/reservation.php
@@ -1,10 +1,31 @@
 
 <?php
 
+
 class reservation{
 
-  
-}
+    private $recibe;
+
+    function DataReservation(){
+        $name = $_POST['name'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+
+        
+    }
+    
+
+     function confirmacion(){
+
+        $recibe = new Mesa();
+        $recibe -> status;
+        
+
+        if (isset($_POST)&& $recibe ->status= true ){
+          echo "Reservacion realizada";
+       } 
+
+}}
 
 
 
